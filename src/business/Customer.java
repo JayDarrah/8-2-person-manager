c9c67395236e2a31dac069ec4aeee25dabeae829
Denis Zamorski <deniszamorski1@gmail.com>
2019-05-28 12:02:04 -0400

    customer

--- src/business/Customer.java
+++ src/business/Customer.java
@@ -2,4 +2,29 @@ package business;
 
 public class Customer extends Person {
 
+	private String customerNumber;
+
+	public Customer(String firstName, String lastName, String customerNumber) {
+		super(firstName, lastName);
+		this.customerNumber = customerNumber;
+	}
+
+	public Customer(String firstName, String lastName) {
+		super(firstName, lastName);
+	}
+
+	public String getCustomerNumber() {
+		return customerNumber;
+	}
+
+	public void setCustomerNumber(String customerNumber) {
+		this.customerNumber = customerNumber;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + "\nCustomer Number: " + customerNumber;
+	}
+	
+	
 }
