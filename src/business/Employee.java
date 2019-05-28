3edf945a051b921554ce453dd7cc03455e66996c
Denis Zamorski <deniszamorski1@gmail.com>
2019-05-28 13:24:23 -0400

    employee

--- src/business/Employee.java
+++ src/business/Employee.java
@@ -2,4 +2,29 @@ package business;
 
 public class Employee extends Person {
 
+	private String ssn;
+
+	public Employee(String firstName, String lastName, String ssn) {
+		super(firstName, lastName);
+		this.ssn = ssn;
+	}
+
+	public Employee(String firstName, String lastName) {
+		super(firstName, lastName);
+	}
+
+	public String getSsn() {
+		return ssn;
+	}
+
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
+
+	@Override
+	public String toString() {
+		return super.toString() + "\nEmployee: " + ssn;
+	}
+	
+	
 }
